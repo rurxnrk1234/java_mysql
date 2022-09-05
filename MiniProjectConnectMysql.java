4a08a2544d0b3d7d0a424ceb1a936cc5e84463b9
rurxnrk1234 <rurxnrk1234@naver.com>
2022-09-05 09:44:44 +0900

    message

---
+++ MiniProjectConnectMysql.java
@@ -0,0 +1,21 @@
+public class MiniProjectConnectMysql {
+    public static void main(String[] args) {
+        // conect
+        // java 서버:
+        // java 유저:
+        // java 비번:
+        // java 커리
+
+        // java 서버 유저 비번
+        // java 커리
+        // 프린트
+
+
+        // 캐치문
+
+        
+        return;
+    }
+
+}
+
